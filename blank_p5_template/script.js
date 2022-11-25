b40868f1d800f974a37a7b0c75103e1b67965379
jiawen21 <jl12592@nyu.edu>
2022-11-25 10:51:15 +0800

    upload the project file

--- blank_p5_template/script.js
+++ blank_p5_template/script.js
@@ -1,10 +1,21 @@
 console.log("my script is loaded successfully")
 
-function setup(){
-
-    createCanvas (300,300);
-    background (30,200);
-}
-
-function draw(){
-}
+function setup() {
+    let canvas = createCanvas(400, 400);
+    canvas.parent("myContainer");
+    background(255,0,0)
+  }
+  
+  function draw() {
+    background(240);
+  
+  noStroke();
+  
+ //fill with transparency (4th arguement)
+  fill(200,0,10,100);
+  rect(200, 200.90,50);
+  
+  //fill with transparency (4th arguement)
+  fill(0,100,255,50);
+  ellipse(180,190,90,50)
+  }
