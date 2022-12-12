963d3d7f3f489319864b3503ad22c88b8482a7eb
jiawen21 <jl12592@nyu.edu>
2022-12-12 23:53:53 +0800

    final project

---
+++ project/script.js
@@ -0,0 +1,100 @@
+// here is my p5
+
+let particles = [];
+
+function setup() {
+  createCanvas(1500, 1000);
+  background(0);
+
+  for (let i = 0; i < 350; i++) {
+    particles[i] = new Particle(random(width), random(-100, 0) );
+  }
+}
+
+function draw() {
+  background(0);
+
+  for (let i = 0; i < particles.length; i++) {
+    let p = particles[i]; // each object
+    p.move();
+    p.reappear();
+    p.display();
+  }
+
+
+}
+
+
+
+class Particle {
+  constructor(x, y) {
+    // properties (variables)
+    this.x = x;
+    this.y = y;
+    this.xSpd = 0;
+    this.ySpd = random(0.5, 2.0);
+    this.dia = random(3, 10);
+    this.r = 255;
+    this.g = 255;
+    this.b = 255;
+  }
+  // methods (functions)
+  move() {
+    this.x += this.xSpd;
+    this.y += this.ySpd;
+  }
+  zigzag() {
+    this.x += random(-0.5, 0.5);
+  }
+  reappear() {
+    if (this.x < 0) {
+      this.x = width;
+    }
+    else if (this.x > width) {
+      this.x = 0;
+    }
+    if (this.y < 0) {
+      //this.x = height;
+    }
+    else if (this.y > height) {
+      this.y = 0;
+    }
+  }
+  display() {
+    push();
+    noStroke();
+    fill(this.r, this.g, this.b, 100);
+    circle(this.x, this.y, this.dia);
+    circle(this.x, this.y, this.dia * 0.6);
+    circle(this.x, this.y, this.dia * 0.3);
+    pop();
+  }
+}
+
+function showImg(obj) {
+  var files = obj.files
+  // document.getElementById("imgContainer").innerHTML = getImgsByUrl(files)
+  getImgsByFileReader(document.getElementById("imgContainer"), files)
+}
+
+function getImgsByUrl(files) {
+  var elements = ''
+  for (var i = 0; i < files.length; i++) {
+      var url = window.URL.createObjectURL(files[i])
+      elements += "<img src='"+ url + "' style='width: 400px; height: 400px; vertical-align: middle; margin-right: 5px;' />"
+  }
+  return elements
+}
+
+function getImgsByFileReader(el, files) {
+  for (var i = 0; i < files.length; i++) {
+      let img = document.createElement('img')
+      img.setAttribute('style', 'width: 250px; height: 250px; vertical-align: middle; margin-right: 10px;')
+      el.appendChild(img)
+      var reader = new FileReader()
+      reader.onload = function(e) {
+          img.src = e.target.result
+      }
+      reader.readAsDataURL(files[i]) 
+  }
+}
---
+++ project/script1.js
@@ -0,0 +1,58 @@
+function setup() {
+    createCanvas(windowWidth, windowHeight);
+    background(0);
+
+
+  }
+  function draw() {
+   
+
+
+     if (keyIsPressed && key=="a") {
+      stroke(11,107,49);
+      strokeWeight(6);
+      line(pmouseX, pmouseY, mouseX, mouseY);  
+    }
+   
+   if (keyIsPressed && key=="b") {
+      stroke(255,100,120);
+      strokeWeight(6);
+      line(pmouseX, pmouseY, mouseX, mouseY);  
+    }
+    
+    if (keyIsPressed && key=="c") {
+      fill(204,102,0);
+      noStroke()
+      ellipse(pmouseX,pmouseY,20,20);}
+    
+     if (keyIsPressed && key=="d") {
+      stroke(102,178,255);
+      strokeWeight(6);
+      line(pmouseX, pmouseY, mouseX, mouseY);  
+    }
+    
+    if (keyIsPressed && key=="e") { 
+        stroke(248,255,1);
+         strokeWeight(6);
+         line(pmouseX, pmouseY, mouseX, mouseY); 
+       }
+
+       push()
+       fill((255), mouseX);
+       textSize(30);
+       text('Draw a Christmas Tree', 40, 80)
+       pop()
+   
+       push()
+       fill(255, mouseX);
+       textSize(25);
+         text('GREEN-press mouse and key a', 40, 110)
+         text('RED-press mouse and key b', 40, 140)
+         text('ORANGE-press mouse and key c', 40, 170)
+         text('BLUE-press mouse and key d', 40, 200)
+         text('YELLOW-press mouse and key e', 40, 230)
+         pop()
+ 
+
+  
+    }
